32d764adad10432d97f2c91fc5173d497ebd49be
rafalebre <rafaellebre100@gmail.com>
2023-07-18 20:59:47 +0000

    trades details added

--- src/front/js/component/Trades.jsx
+++ src/front/js/component/Trades.jsx
@@ -34,7 +34,7 @@ const Trades = () => {
       {receivedTrades.map((proposal) => (
         <div key={proposal.id}>
           <p>
-            User "{proposal.sender_name}" has offered "{proposal.sender_item_name}" in exchange for "{proposal.receiver_item_name}"
+            Check the offer: "{proposal.sender_item_name}" in exchange for your "{proposal.receiver_item_name}"
           </p>
           {proposal.status === "Pending" && (
             <div>
