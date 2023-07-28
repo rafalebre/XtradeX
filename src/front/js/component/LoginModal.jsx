2b1e24e978d2ebf4ad5a03ce1e65712fcb2252cb
rafalebre <rafaellebre100@gmail.com>
2023-07-28 06:26:05 +0000

    styles login modal

--- src/front/js/component/LoginModal.jsx
+++ src/front/js/component/LoginModal.jsx
@@ -1,47 +1,69 @@
 import React, { useState, useContext } from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Alert } from "react-bootstrap";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
+import './LoginModal.css';
 
 const LoginModal = ({ show, handleClose }) => {
   const { actions } = useContext(Context);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showAlert, setShowAlert] = useState(false);  
+  const [alertMessage, setAlertMessage] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = (event) => {
     event.preventDefault();
-    // Aqui, adicionar a lógica para autenticar o usuário
-    // Chamando uma função actions para lidar com o login:
+    if (!validateEmail(email)) {
+      setAlertMessage("Please insert a valid email");
+      setShowAlert(true);
+      return;
+    }
+    if (password === "") {
+      setAlertMessage("Please insert a password");
+      setShowAlert(true);
+      return;
+    }
     actions.loginUser(email, password, navigate, handleClose);
   };
 
+  const validateEmail = (email) => {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
   return (
-    <Modal show={show} onHide={handleClose}>
+    <Modal show={show} onHide={handleClose} className="loginModal">
       <Modal.Header closeButton>
         <Modal.Title>Login</Modal.Title>
       </Modal.Header>
       <form onSubmit={handleLogin}>
         <Modal.Body>
+          {showAlert && (
+            <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
+              <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+              <p>{alertMessage}</p>
+            </Alert>
+          )}
           <div className="mb-3">
-            <label htmlFor="email" className="form-label">
+            <label htmlFor="email" className="form-label login-label">
               Email address
             </label>
             <input
               type="email"
-              className="form-control"
+              className="form-control login-input"
               id="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-3">
-            <label htmlFor="password" className="form-label">
+            <label htmlFor="password" className="form-label login-label">
               Password
             </label>
             <input
               type="password"
-              className="form-control"
+              className="form-control login-input"
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
@@ -52,7 +74,7 @@ const LoginModal = ({ show, handleClose }) => {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" className="login-submit">
             Login
           </Button>
         </Modal.Footer>
