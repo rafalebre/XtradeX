51e23717ba1c05d2107bed17cf4b8140adbc5a1d
rafalebre <rafaellebre100@gmail.com>
2023-07-28 13:13:37 +0000

    UserItems styled

--- src/front/js/component/UserItems.jsx
+++ src/front/js/component/UserItems.jsx
@@ -1,36 +1,41 @@
 import React, { useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
+import "./UserItems.css";
 
 const UserItems = () => {
-    const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-    useEffect(() => {
-        actions.fetchUserItems(); // Fetch user items on component mount
-    }, []);
+  useEffect(() => {
+    actions.fetchUserItems(); // Fetch user items on component mount
+  }, []);
 
-    return (
-        <div>
-            <h2>My Products</h2>
-            <ul>
-                {store.userProducts &&
-                    store.userProducts.map((product) => (
-                        <li key={product.id}>
-                            {product.name} - Product - {product.description}
-                        </li>
-                    ))}
-            </ul>
+  return (
+    <div className="user-items-container">
+      <div className="user-items-list">
+        <h2 className="user-items-title">My Products</h2>
+        <ul>
+          {store.userProducts &&
+            store.userProducts.map((product) => (
+              <li key={product.id} className="user-item">
+                {product.name} - {product.description} - {product.currency}{product.estimated_value}
+              </li>
+            ))}
+        </ul>
+      </div>
 
-            <h2>My Services</h2>
-            <ul>
-                {store.userServices &&
-                    store.userServices.map((service) => (
-                        <li key={service.id}>
-                            {service.name} - Service - {service.description}
-                        </li>
-                    ))}
-            </ul>
-        </div>
-    );
+      <div className="user-items-list">
+        <h2 className="user-items-title">My Services</h2>
+        <ul>
+          {store.userServices &&
+            store.userServices.map((service) => (
+              <li key={service.id} className="user-item">
+                {service.name} - {service.description} - {service.currency}{service.estimated_value}
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default UserItems;
