e5ec372ce091c1b69ec62b9702b748dd6682e059
rafalebre <rafaellebre100@gmail.com>
2023-08-11 09:18:29 +0000

    navigate removed from home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,38 +8,11 @@ import HomeSearch from "../component/HomeSearch.jsx";
 
 export const Home = () => {
     const { actions } = useContext(Context);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
     const [showAlert, setShowAlert] = useState(false);  
     const [alertMessage, setAlertMessage] = useState("");
     const navigate = useNavigate();
 
-    const handleRegister = (event) => {
-        event.preventDefault();
-
-        if (!validateEmail(email)) {
-            setAlertMessage("Please insert a valid e-mail.");
-            setShowAlert(true);
-            return;
-        }
-        if (password === "") {
-            setAlertMessage("Please insert a password.");
-            setShowAlert(true);
-            return;
-        }
-        actions.registerUser(email, password)
-            .then(success => {
-                if (success) {
-                    navigate('/myinfo');
-                }
-            });
-    };
-
-    const validateEmail = email => {
-        var re = /\S+@\S+\.\S+/;
-        return re.test(email);
-    };
-
+    
     return (
         <div className="home-container">
             {showAlert && (
@@ -57,28 +30,7 @@ export const Home = () => {
                 <HomeSearch /> {/* Adicionando a ferramenta de busca completa */}
             </div>
 
-            <div className="row">
-                <div className="col-6">
-                    <h1>How does it work?</h1>
-                    <ol>
-                        <li>You register a product or service.</li>
-                        <li>You search for other services and products.</li>
-                        <li>You propose a trade.</li>
-                        <li>You also receive proposals.</li>
-                        <li>You become part of the X-Trade-X community.</li>
-                    </ol>
-                </div>
-                <div className="col-6">
-                    <div className="d-flex flex-column align-items-center justify-content-center">
-                        <h1>REGISTER</h1>
-                        <form onSubmit={handleRegister}>
-                            <input type="email" className="form-control mt-2" placeholder="Email" onChange={e => setEmail(e.target.value)} />
-                            <input type="password" className="form-control mt-2" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-                            <button className="btn btn-primary mt-2" type="submit">Submit</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     );
 };
