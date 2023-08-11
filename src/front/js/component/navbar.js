c9edbf7fac86e071587fa2bfbce4d7f99fd7ca11
rafalebre <rafaellebre100@gmail.com>
2023-08-11 07:36:54 +0000

    working on home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import LoginModal from "./LoginModal.jsx";
 import { useNavigate } from "react-router-dom";
-import Logo from "../../img/Logo.png";
+import Logo from "../../img/logo.png";
 import "./navbar.css";
 
 export const Navbar = () => {
