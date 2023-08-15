750815ff8829d4fd9d4ef104e46c9735d077fd6a
rafalebre <rafaellebre100@gmail.com>
2023-08-15 20:22:22 +0000

    fixed button levels in online services

--- src/front/js/component/OnlineServicesSearch.jsx
+++ src/front/js/component/OnlineServicesSearch.jsx
@@ -11,6 +11,7 @@ const OnlineServicesSearch = () => {
   const { store, actions } = useContext(Context);
   const [showTradeProposalModal, setShowTradeProposalModal] = useState(false);
   const [selectedService, setSelectedService] = useState(null);
+  const [expandedDescriptions, setExpandedDescriptions] = useState({});
 
   useEffect(() => {
     actions.getServiceCategories();
@@ -33,7 +34,7 @@ const OnlineServicesSearch = () => {
 
   const handleSearch = (e) => {
     e.preventDefault();
-  
+
     if (specificSearch) {
       actions.fetchOnlineServicesByName(searchTerm);
     } else {
@@ -51,71 +52,92 @@ const OnlineServicesSearch = () => {
     ? store.serviceSubcategories.filter((sub) => sub.category_id == selectedCategory)
     : [];
 
-    return (
-      <div className="online-search-container">
-        <button className="search-button" onClick={handleSpecificSearchToggle}>
-          Specific Item Search
-        </button>
-  
-        {specificSearch ? (
-          <div className="input-container">
-            <input
-              type="text"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              placeholder="Enter item name..."
-              className="dropdown"
-            />
-            <button className="search-button" onClick={handleSearch}>
-              Search
-            </button>
-          </div>
-        ) : (
-          <form className="category-form" onSubmit={handleSearch}>
-            <select className="dropdown" value={selectedCategory} onChange={handleCategoryChange}>
-              <option value="">Select Category</option>
-              {store.serviceCategories.map((category) => (
-                <option key={category.id} value={category.id}>
-                  {category.name}
-                </option>
-              ))}
-            </select>
-  
-            <select className="dropdown" value={selectedSubcategory} onChange={(e) => setSelectedSubcategory(e.target.value)}>
-              <option value="">Select Subcategory</option>
-              {filteredSubcategories.map((subcategory) => (
-                <option key={subcategory.id} value={subcategory.id}>
-                  {subcategory.name}
-                </option>
-              ))}
-            </select>
-            <button className="search-button" type="submit">
-              Search
-            </button>
-          </form>
-        )}
-  
-        <div className="online-list">
-          {store.onlineServices.map((service, index) => (
-            <div key={index} className="online-card">
-              <h3>{service.name}</h3>
-              <p>{service.description}</p>
-              <p>{service.currency} {service.estimated_value}</p>
-              <button className="propose-button" onClick={() => handleProposeDeal(service)}>Propose a Deal</button> 
-            </div>
-          ))}
-        </div>
-  
-        {showTradeProposalModal && (
-          <TradeProposal
-            show={showTradeProposalModal}
-            handleClose={() => setShowTradeProposalModal(false)}
-            itemToTrade={selectedService}
-            itemType="service"
-          />
-        )}
-      </div>
-    );
+  const handleExpandDescription = (serviceId) => {
+    setExpandedDescriptions((prevState) => ({
+      ...prevState,
+      [serviceId]: !prevState[serviceId]
+    }));
   };
-  
-  export default OnlineServicesSearch;
+
+  return (
+    <div className="online-search-container">
+      <button className="search-btn" onClick={handleSpecificSearchToggle}>
+        Specific Item Search
+      </button>
+
+      {specificSearch ? (
+        <div className="input-container">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Enter item name..."
+            className="dropdown-input"
+          />
+          <button className="search-btn" onClick={handleSearch}>
+            Search
+          </button>
+        </div>
+      ) : (
+        <form className="category-form" onSubmit={handleSearch}>
+          <select className="dropdown-input" value={selectedCategory} onChange={handleCategoryChange}>
+            <option value="">Select Category</option>
+            {store.serviceCategories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+
+          <select className="dropdown-input" value={selectedSubcategory} onChange={(e) => setSelectedSubcategory(e.target.value)}>
+            <option value="">Select Subcategory</option>
+            {filteredSubcategories.map((subcategory) => (
+              <option key={subcategory.id} value={subcategory.id}>
+                {subcategory.name}
+              </option>
+            ))}
+          </select>
+          <button className="search-btn" type="submit">
+            Search
+          </button>
+        </form>
+      )}
+
+      <div className="online-list">
+        {store.onlineServices.map((service, index) => (
+          <div key={index} className="online-card">
+            <div className="card-content">
+              <h3>{service.name}</h3>
+              <p className={`description ${expandedDescriptions[service.id] ? 'expanded' : ''}`}>
+                {service.description}
+              </p>
+              {service.description.length > 100 && (
+                <button
+                  className="see-more-btn"
+                  onClick={() => handleExpandDescription(service.id)}
+                >
+                  {expandedDescriptions[service.id] ? 'See Less' : 'See More'}
+                </button>
+              )}
+            </div>
+            <div className="card-action">
+              <p>{service.currency} {service.estimated_value}</p>
+              <button className="propose-btn" onClick={() => handleProposeDeal(service)}>Propose a Deal</button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {showTradeProposalModal && (
+        <TradeProposal
+          show={showTradeProposalModal}
+          handleClose={() => setShowTradeProposalModal(false)}
+          itemToTrade={selectedService}
+          itemType="service"
+        />
+      )}
+    </div>
+  );
+};
+
+export default OnlineServicesSearch;
