2b8974f2119f775df3655d0f56c4414e3d8f33cf
rafalebre <rafaellebre100@gmail.com>
2023-08-15 20:02:39 +0000

    about papiro paper

--- src/front/js/pages/About.jsx
+++ src/front/js/pages/About.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
 import "./About.css";
+import About3 from "../../img/About2.png";
 
 const About = () => {
-  return <div className="about-container">
-   <h1 className="about-text">
-    
-    Welcome to our platform! 
-
-At X-Trade-X, we believe in creating a world where waste is minimized, communities come together, and knowledge and goods are shared freely. Our mission is to empower individuals to unlock the true value of their skills, products, and services, fostering a thriving ecosystem of exchange and collaboration. <br/>
-
-Our platform provides a unique space where users can showcase their diverse array of products and services. By enabling seamless exchanges between community members, we strive to reduce waste and promote sustainability. Together, we can move towards a more responsible and mindful consumption pattern, benefiting both individuals and the planet we call home.<br/>
-
-Beyond the tangible benefits, X-Trade-X is also about building connections. We encourage interaction and dialogue among users, facilitating meaningful interactions that strengthen the sense of community. As people come together to trade, share, and learn from one another, we envision a world that is more closely connected and understanding.<br/>
-
-Education is a core pillar of our platform. We understand the immense value of knowledge and expertise. Therefore, we encourage users to leverage their skills as a medium of exchange. By sharing knowledge, our community members uplift one another, creating a positive cycle of personal and collective growth.<br/>
-
-At X-Trade-X, we value the potential of every individual to contribute meaningfully to society. We believe that everyone has something unique to offer, and by embracing the spirit of reciprocity, we can unlock boundless possibilities. <br/>
-
-Join us on this transformative journey. Whether you are a seasoned expert or a passionate learner, there is a place for you here. Together, let's take a step towards a more sustainable, connected, and enlightened world. Embrace the power of exchange, and let's shape a brighter future together. <br/>
-
-Thank you for being part of the X-Trade-X community. Together, we can make a difference. <br/>
-
-Warm regards, <br/>
-
-Rafael <br/>
-Founder, X-Trade-X
-  </h1> </div>;
+  return (
+    <div className="about-container">
+      <img src={About3} alt="About Us" className="about-image" />
+    </div>
+  );
 };
 
 export default About;
