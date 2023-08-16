348fc5fb9ca0fcb5f71a0225d3d8b01c6f66d590
rafalebre <rafaellebre100@gmail.com>
2023-08-16 23:10:46 +0000

    selected items - profile

--- src/front/js/pages/Profile.jsx
+++ src/front/js/pages/Profile.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState } from "react"; 
-import { Context } from "../store/appContext"; 
+import React, { useContext, useState, useEffect } from "react";
+import { Context } from "../store/appContext";
 import Sidebar from "./Sidebar.jsx";
 import AddProducts from "../component/AddProducts.jsx";
 import AddService from "../component/AddService.jsx";
@@ -11,18 +11,11 @@ import Favorites from "../component/Favorites.jsx";
 import OnlineServicesSearch from "../component/OnlineServicesSearch.jsx";
 
 const Profile = () => {
-  
   const [showInitialMessage, setShowInitialMessage] = useState(true);
-  // Estado para rastrear a opção de menu selecionada
   const [selectedMenu, setSelectedMenu] = useState(null);
-  
-  // Use useContext para acessar o estado global da loja
-  const { actions, store } = useContext(Context); 
-
-  // Estado para armazenar o intervalId
+  const { actions, store } = useContext(Context);
   const [intervalId, setIntervalId] = useState(null);
 
-  // Função para atualizar a opção de menu selecionada
   const handleMenuSelection = (menu) => {
     setSelectedMenu(menu);
     setShowInitialMessage(false);
@@ -42,20 +35,18 @@ const Profile = () => {
 
   return (
     <div className="profile-container">
-      {/* Passa a função de callback para o componente Sidebar */}
       <Sidebar 
         onMenuSelect={handleMenuSelection} 
+        selectedMenu={selectedMenu} 
         newTradesCount={store.new_sent_trades.length + store.new_received_trades.length}
       />
       <div className="content-area">
-      {showInitialMessage && (
+        {showInitialMessage && (
           <div className="initial-message-container">
             <h2>Choose an option</h2>
             <div className="arrow-right" />
           </div>)}
-        {/* Renderiza o componente AddProduct se 'Add a product' estiver selecionado */}
         {selectedMenu === "addProduct" && <AddProducts />}
-        {/* Aqui iremos adicionando condições adicionais para outras opções de menu */}
         {selectedMenu === "addService" && <AddService />}
         {selectedMenu === "search" && <Search />}
         {selectedMenu === "onlineSearch" && <OnlineServicesSearch />}
--- src/front/js/pages/Sidebar.jsx
+++ src/front/js/pages/Sidebar.jsx
@@ -4,13 +4,11 @@ import "./Profile.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCamera } from '@fortawesome/free-solid-svg-icons';
 
-
-const Sidebar = ({ onMenuSelect, newTradesCount }) => {
+const Sidebar = ({ onMenuSelect, selectedMenu, newTradesCount }) => {
   const { store } = useContext(Context);
   const [profilePic, setProfilePic] = useState(() => localStorage.getItem("profilePic") || null);
 
   useEffect(() => {
-    // Carregar a foto de perfil do localStorage quando o componente for montado
     const storedProfilePic = localStorage.getItem("profilePic");
     if (storedProfilePic) {
       setProfilePic(storedProfilePic);
@@ -23,7 +21,6 @@ const Sidebar = ({ onMenuSelect, newTradesCount }) => {
     reader.onloadend = () => {
       const dataURL = reader.result;
       setProfilePic(dataURL);
-      // Armazenar a foto de perfil no localStorage
       localStorage.setItem("profilePic", dataURL);
     };
     if (file) {
@@ -52,19 +49,59 @@ const Sidebar = ({ onMenuSelect, newTradesCount }) => {
         )}
       </div>
       <ul className="sidebar-menu">
-        <li onClick={() => onMenuSelect("addProduct")}>Add a product</li>
-        <li onClick={() => onMenuSelect("addService")}>Add a service</li>
-        <li onClick={() => onMenuSelect("search")}>Search</li>
-        <li onClick={() => onMenuSelect("onlineSearch")}>Online Services</li>
-        <li onClick={() => onMenuSelect("userItems")}>My Products / Services</li>
-        <li onClick={() => onMenuSelect("trades")}>Trades</li>
+        <li
+          onClick={() => onMenuSelect("addProduct")}
+          className={selectedMenu === "addProduct" ? "selected" : ""}
+        >
+          Add a product
+        </li>
+        <li
+          onClick={() => onMenuSelect("addService")}
+          className={selectedMenu === "addService" ? "selected" : ""}
+        >
+          Add a service
+        </li>
+        <li
+          onClick={() => onMenuSelect("search")}
+          className={selectedMenu === "search" ? "selected" : ""}
+        >
+          Search
+        </li>
+        <li
+          onClick={() => onMenuSelect("onlineSearch")}
+          className={selectedMenu === "onlineSearch" ? "selected" : ""}
+        >
+          Online Services
+        </li>
+        <li
+          onClick={() => onMenuSelect("userItems")}
+          className={selectedMenu === "userItems" ? "selected" : ""}
+        >
+          My Products / Services
+        </li>
+        <li
+          onClick={() => onMenuSelect("trades")}
+          className={selectedMenu === "trades" ? "selected" : ""}
+        >
+          Trades
+        </li>
         {newTradesCount > 0 && (
           <li>
             <span className="notification">{newTradesCount}</span>
           </li>
         )}
-        <li onClick={() => onMenuSelect("wishlist")}>Wishlist</li>
-        <li onClick={() => onMenuSelect("favorites")}>Favorites</li>
+        <li
+          onClick={() => onMenuSelect("wishlist")}
+          className={selectedMenu === "wishlist" ? "selected" : ""}
+        >
+          Wishlist
+        </li>
+        <li
+          onClick={() => onMenuSelect("favorites")}
+          className={selectedMenu === "favorites" ? "selected" : ""}
+        >
+          Favorites
+        </li>
       </ul>
     </div>
   );
