67bd1174205ad58ac49e58afb20f9c699ee6bf17
rafalebre <rafaellebre100@gmail.com>
2023-08-16 20:16:09 +0000

    TradeDetails adjustments 1

--- src/front/js/component/TradeDetails.jsx
+++ src/front/js/component/TradeDetails.jsx
@@ -1,9 +1,9 @@
 import React, { useContext, useState, useEffect } from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Row, Col } from "react-bootstrap";
 import { Context } from "../store/appContext";
 import "./TradeDetails.css";
 
-const TradeDetails = ({ show, handleClose, trade, refreshTrades }) => {  // refreshTrades adicionado aqui
+const TradeDetails = ({ show, handleClose, trade, refreshTrades }) => {
   const { store, actions } = useContext(Context);
   const [tradeStatus, setTradeStatus] = useState(trade.status);
 
@@ -14,13 +14,13 @@ const TradeDetails = ({ show, handleClose, trade, refreshTrades }) => {  // refr
   const handleAccept = async () => {
     await actions.handleAcceptProposal(trade.id);
     setTradeStatus("Accepted");
-    refreshTrades();  // Chamando refreshTrades após aceitar um trade
+    refreshTrades();
   };
 
   const handleDecline = async () => {
     await actions.handleDeclineProposal(trade.id);
     setTradeStatus("Declined");
-    refreshTrades();  // Chamando refreshTrades após recusar um trade
+    refreshTrades();
   };
 
   const isReceivedTrade = store.received_trades.some(
@@ -29,52 +29,64 @@ const TradeDetails = ({ show, handleClose, trade, refreshTrades }) => {  // refr
 
   return (
     <Modal show={show} onHide={handleClose} className="TradeDetails">
-      <Modal.Header closeButton>
-        <Modal.Title>Trade Details</Modal.Title>
+      <Modal.Header closeButton className="modal-header">
+        <Modal.Title className="modal-title">Trade Details</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Sender Item: {trade.sender_item_name}</h4>
-        {trade.sender_product_details && (
-          <div>
-            <h5>Product Details:</h5>
-            <p>Name: {trade.sender_product_details.name}</p>
-            <p>Description: {trade.sender_product_details.description}</p>
-            <p>Condition: {trade.sender_product_details.condition}</p>
-            <p>Estimated Value: {trade.sender_product_details.estimated_value}</p>
-            <p>Location: {trade.sender_product_details.location}</p>
-          </div>
-        )}
-        {trade.sender_service_details && (
-          <div>
-            <h5>Service Details:</h5>
-            <p>Name: {trade.sender_service_details.name}</p>
-            <p>Description: {trade.sender_service_details.description}</p>
-            <p>Estimated Value: {trade.sender_service_details.estimated_value}</p>
-            <p>Location: {trade.sender_service_details.location}</p>
-          </div>
-        )}
-        <h4>Receiver Item: {trade.receiver_item_name}</h4>
-        {trade.receiver_product_details && (
-          <div>
-            <h5>Product Details:</h5>
-            <p>Name: {trade.receiver_product_details.name}</p>
-            <p>Description: {trade.receiver_product_details.description}</p>
-            <p>Condition: {trade.receiver_product_details.condition}</p>
-            <p>Estimated Value: {trade.receiver_product_details.estimated_value}</p>
-            <p>Location: {trade.receiver_product_details.location}</p>
-          </div>
-        )}
-        {trade.receiver_service_details && (
-          <div>
-            <h5>Service Details:</h5>
-            <p>Name: {trade.receiver_service_details.name}</p>
-            <p>Description: {trade.receiver_service_details.description}</p>
-            <p>Estimated Value: {trade.receiver_service_details.estimated_value}</p>
-            <p>Location: {trade.receiver_service_details.location}</p>
-          </div>
-        )}
-        <p>Message: {trade.message}</p>
-        <p>Status: {tradeStatus}</p>
+        <Row>
+          <Col lg={6}>
+            <div className="details-section">
+              <h4>Sender Item: {trade.sender_item_name}</h4>
+              {trade.sender_product_details && (
+                <div>
+                  <h5>Product Details:</h5>
+                  <p><strong>Name:</strong> {trade.sender_product_details.name}</p>
+                  <p><strong>Description:</strong> {trade.sender_product_details.description}</p>
+                  <p><strong>Estimated Value:</strong> {trade.sender_product_details.estimated_value}</p>
+                  <p><strong>Location:</strong> {trade.sender_product_details.location}</p>
+                </div>
+              )}
+              {trade.sender_service_details && (
+                <div>
+                  <h5>Service Details:</h5>
+                  <p><strong>Name:</strong> {trade.sender_service_details.name}</p>
+                  <p><strong>Description:</strong> {trade.sender_service_details.description}</p>
+                  <p><strong>Estimated Value:</strong> {trade.sender_service_details.estimated_value}</p>
+                  <p><strong>Location:</strong> {trade.sender_service_details.location}</p>
+                </div>
+              )}
+            </div>
+          </Col>
+          <Col lg={6}>
+            <div className="details-section">
+              <h4>Receiver Item: {trade.receiver_item_name}</h4>
+              {trade.receiver_product_details && (
+                <div>
+                  <h5>Product Details:</h5>
+                  <p><strong>Name:</strong> {trade.receiver_product_details.name}</p>
+                  <p><strong>Description:</strong> {trade.receiver_product_details.description}</p>
+                  <p><strong>Estimated Value:</strong> {trade.receiver_product_details.estimated_value}</p>
+                  <p><strong>Location:</strong> {trade.receiver_product_details.location}</p>
+                </div>
+              )}
+              {trade.receiver_service_details && (
+                <div>
+                  <h5>Service Details:</h5>
+                  <p><strong>Name:</strong> {trade.receiver_service_details.name}</p>
+                  <p><strong>Description:</strong> {trade.receiver_service_details.description}</p>
+                  <p><strong>Estimated Value:</strong> {trade.receiver_service_details.estimated_value}</p>
+                  <p><strong>Location:</strong> {trade.receiver_service_details.location}</p>
+                </div>
+              )}
+            </div>
+          </Col>
+        </Row>
+        <Row className="message-status-row">
+          <Col className="text-center">
+            <p><strong>Message:</strong> {trade.message}</p>
+            <p><strong>Status:</strong> {tradeStatus}</p>
+          </Col>
+        </Row>
       </Modal.Body>
       <Modal.Footer>
         {isReceivedTrade && tradeStatus === "Pending" && (
