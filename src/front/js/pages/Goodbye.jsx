f5e5f002950cc8b7e08f3002ad14c49121851faf
rafalebre <rafaellebre100@gmail.com>
2023-08-16 22:19:45 +0000

    back to home - goodbye

--- src/front/js/pages/Goodbye.jsx
+++ src/front/js/pages/Goodbye.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import "./Goodbye.css"; // Importando o arquivo CSS criado
+import { useNavigate } from 'react-router-dom'; 
+import "./Goodbye.css"; 
 
 const Goodbye = () => {
+  const navigate = useNavigate(); 
+
+  const handleBackToHome = () => {
+    navigate('/'); 
+  };
+
   return (
     <div className="goodbye-container">
       <h1 className="goodbye-title">You've been logged out.<br/>See you Soon!</h1>
-      <button className="goodbye-button">Back to Home</button>
+      <button className="goodbye-button" onClick={handleBackToHome}>Back to Home</button>
     </div>
   );
 };
