e42e217fa05b901ac85121ac7be346205aa2afc8
rafalebre <rafaellebre100@gmail.com>
2023-08-17 06:42:29 +0000

    favorites styled

--- src/front/js/component/Favorites.jsx
+++ src/front/js/component/Favorites.jsx
@@ -9,25 +9,16 @@ const Favorites = () => {
   const [selectedItemType, setSelectedItemType] = useState(null);
   const [showDetails, setShowDetails] = useState(false);
   const [showTradeProposal, setShowTradeProposal] = useState(false);
-  const [favoriteDetails, setFavoriteDetails] = useState(null); // Estado para armazenar os detalhes do favorito
   const { store, actions } = useContext(Context);
 
   useEffect(() => {
     actions.getFavorites();
   }, []);
 
-  const handleOpenDetails = async (item, itemType) => {
+  const handleOpenDetails = (item, itemType) => {
     setSelectedItem(item);
     setSelectedItemType(itemType);
     setShowDetails(true);
-
-    // Carregar os detalhes do favorito usando o ID do item
-    try {
-      const favorite = await actions.getFavoriteById(item.id);
-      setFavoriteDetails(favorite);
-    } catch (error) {
-      console.error("Error loading favorite details:", error);
-    }
   };
 
   const handleCloseDetails = () => {
@@ -48,75 +39,43 @@ const Favorites = () => {
     actions.removeFavorite(itemId);
   };
 
-  const renderItems = (items) => {
-    if (!items) {
-      return null;
-    }
-
-    return items.map((item, index) => (
-      <div key={index} className="item-card">
-        <h3>{item.details.name}</h3>
-        {item.type === "product" && <p>Condition: {item.details.condition}</p>}
-        <p>Estimated Value: {item.details.estimated_value}{item.details.currency}</p>
-        <button
-          className="orange-button"
-          onClick={() =>
-            handleOpenDetails(item.details, item.type)
-          }
-        >
-          Check details
-        </button>
-        <button
-          className="orange-button"
-          onClick={() =>
-            handleOpenTradeProposal(item.details, item.type)
-          }
-        >
-          Propose Trade
-        </button>
-        <button
-          className="orange-button"
-          onClick={() => handleRemoveFromFavorites(item.details.id)}
-        >
-          Remove from Favorites
-        </button>
-      </div>
-    ));
-};
-
   return (
-    <div>
-      <h2>Favorites</h2>
-      <div className="item-list">
-        {renderItems(store.favorites, true)}
-      </div>
-      {/* Aqui é onde colocamos o trecho para renderizar o modal */}
-      {showDetails && selectedItem && selectedItemType && (
-        <ItemDetails
-          item={selectedItem}
-          itemType={selectedItemType}
-          onClose={handleCloseDetails}
-        />
-      )}
-      {/* Botão para abrir oTradeProposal */}
-      {selectedItem && (
-        <TradeProposal
-          show={showTradeProposal}
-          handleClose={handleCloseTradeProposal}
-          itemToTrade={selectedItem}
-          itemType={selectedItemType} // passa o tipo de item selecionado 
-        />
-      )}
-      {/* Renderizar os detalhes do favorito */}
-      {showDetails && favoriteDetails && (
-        <div>
-          <h2>Favorite Details</h2>
-          <p>Name: {favoriteDetails.name}</p>
-          {/* Adicione mais detalhes conforme necessário */}
+    <div className="fav-container">
+        <h2 className="fav-heading">Favorites</h2>
+        <div className="fav-list">
+            {store.favorites.map((item, index) => (
+                <div key={index} className="fav-card">
+                    <h3 className="fav-card-title">{item.details.name}</h3>
+                    <p className="fav-card-description">{item.details.description}</p>
+                    {item.type === "product" && <p className="fav-card-condition">Condition: {item.details.condition}</p>}
+                    <p className="fav-card-value">Estimated Value: {item.details.estimated_value}{item.details.currency}</p>
+                    <div className="fav-card-actions">
+                        <button className="fav-details-btn" onClick={() => handleOpenDetails(item.details, item.type)}>Check details</button>
+                        <button className="fav-trade-btn" onClick={() => handleOpenTradeProposal(item.details, item.type)}>Propose Trade</button>
+                        <button className="fav-remove-btn" onClick={() => handleRemoveFromFavorites(item.details.id)}>Remove from Favorites</button>
+                    </div>
+                </div>
+            ))}
         </div>
-      )}
+
+        {/* Modals */}
+        {showDetails && selectedItem && selectedItemType && (
+            <ItemDetails
+                item={selectedItem}
+                itemType={selectedItemType}
+                onClose={handleCloseDetails}
+            />
+        )}
+        {selectedItem && (
+            <TradeProposal
+                show={showTradeProposal}
+                handleClose={handleCloseTradeProposal}
+                itemToTrade={selectedItem}
+                itemType={selectedItemType}
+            />
+        )}
     </div>
-  );
+);
 };
 
 export default Favorites;
